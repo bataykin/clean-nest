534a3cf1ec66fc48e24fe863612ad8bf017fbae8
Sergey Bataykin <bataykin@gmail.com>
2023-03-04 18:02:36 +0200

    uncom all

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { PostsModule } from './posts/posts.module';
 
 import cookieParser from 'cookie-parser';
 
-// const PORT = process.env.PORT || 64000;
+const PORT = process.env.PORT || 64000;
 
 async function bootstrap() {
   // const url = await ngrok.connect(3000);
@@ -24,7 +24,7 @@ async function bootstrap() {
   // // app.use('trust proxy', LoggerMiddleware);
   app.set('trust proxy', 1);
   //
-  // app.use(cookieParser());
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       /*whitelist: true,*/ transform: true,
@@ -44,19 +44,19 @@ async function bootstrap() {
 
   // await app.close()
 
-  await app.listen(3000);
-  // console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
   // console.log('auth type', configService.get('authorization').type)
-  // console.log(
-  //   'REPO_TYPE = ',
-  //   configService.get('REPO_TYPE'),
-  //   ' at ',
-  //   process.env.REPO_TYPE === 'SQL'
-  //     ? configService.get('db.sql')
-  //     : configService.get('db.orm').database,
-  // );
+  console.log(
+    'REPO_TYPE = ',
+    configService.get('REPO_TYPE'),
+    ' at ',
+    process.env.REPO_TYPE === 'SQL'
+      ? configService.get('db.sql')
+      : configService.get('db.orm').database,
+  );
 
-  // console.log('NODE_ENV=', configService.get('NODE_ENV'));
+  console.log('NODE_ENV=', configService.get('NODE_ENV'));
 
   // console.log(url)
 
